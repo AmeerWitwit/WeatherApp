9692875feb78edfeff39f266cb45f7e1d5e6ae0b
Ameer Witwit <ameer.witwit98@gmail.com>
2022-06-28 15:08:38 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ameer Witwit on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
